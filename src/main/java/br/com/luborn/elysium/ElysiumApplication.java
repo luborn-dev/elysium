8bab919601d3d9a3ec204d3c9aa61cdea819001a
Lucas Borges <luborn.dev@gmail.com>
2024-07-08 15:31:15 -0300

    first commit

---
+++ src/main/java/br/com/luborn/elysium/ElysiumApplication.java
@@ -0,0 +1,13 @@
+package br.com.luborn.elysium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElysiumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElysiumApplication.class, args);
+    }
+
+}
